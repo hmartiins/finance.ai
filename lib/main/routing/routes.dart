c051fcc45c9d67ee5c6c3dfcf7863a1b245e8f19
hmartiins <hmartins224@gmail.com>
2025-05-10 23:54:27 -0300

    feat: add additional routes for login, signup, and forgot password

--- lib/main/routing/routes.dart
+++ lib/main/routing/routes.dart
@@ -3,4 +3,7 @@
 abstract final class Routes {
   static const home = '/';
   static const onboarding = '/onboarding';
+  static const login = '/login';
+  static const signup = '/signup';
+  static const forgotPassword = '/forgot-password';
 }
